a1f220af2783cecb208bfa0768dd75e408c6437e
Alexander Leon <1995.Alexander.Leon@gmail.com>
2020-10-01 09:58:43 -0700

    routes and db progress

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const { db } = require('./models');
 const app = express();
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended:false}));
+app.use('/main', require('./router/routes'));
 
-try {
   app.get("/", (req, res) => {
-    res.send("Hello");
+    res.redirect("/main");
   });
-} catch (error) {
-  console.log(error);
-}
 
-app.listen("1337", () => console.log("Listening on Port 1337"));
+app.listen("3000", () => console.log("Listening on Port 3000"));
